efd9313a68bafb019bd91e9d902d940aa9947bab
irenaldo <irenaldojs@gmail.com>
2023-10-23 20:22:56 -0300

    feat: docker + postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRoot({
+      database: process.env.DB_DATABASE,
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      autoLoadEntities: true,
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
